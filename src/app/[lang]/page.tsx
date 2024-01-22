99c153681def8d81ea629013036cf408158f09e3
muhmmed_medhat <muhmmedmedhat0@gmail.com>
2024-01-22 18:41:45 +0200

    first commit

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,22 @@
+import { Locale } from '@/i18n/i18n-config';
+import Header from '../components/header/header';
+import About from '../components/about/about';
+import Document from '../components/document/document';
+import Statistics from '../components/statistics/statistics';
+import Directors from '../components/directors/directors';
+import Funds from '../components/funds/funds';
+import Contact from '../components/contact/contact';
+
+export default function HomePage({params: { lang }}: {params: { lang: Locale }}) {
+  return (
+    <main className="page-wrapper">
+      <Header lang={lang} />
+      <About lang={lang}/>
+      <Document lang={lang}/>
+      <Statistics lang={lang} />
+      {/* <Directors />
+      <Funds />
+      <Contact /> */}
+    </main>
+  );
+}
